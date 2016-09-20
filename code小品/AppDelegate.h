cbee63bc47984e468f796864d2774960cdad10e8
sunkai <1135556017@qq.com>
2016-09-20 16:35:26 +0800

    Initial Commit

---
+++ code小品/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  code小品
+//
+//  Created by flk on 16/9/20.
+//  Copyright © 2016年 flk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ code小品/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  code小品
+//
+//  Created by flk on 16/9/20.
+//  Copyright © 2016年 flk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
